7bd3b00517da4bd6644eeb56b198fead826c2f2d
Alexander Kozlov <Mr.Kozelberg@yandex.ru>
2022-12-08 19:49:36 +0300

    new files

--- source/loop_n_threads.sh
+++ source/loop_n_threads.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 gfortran linear_algebra_cholesky.f90 -o linear_algebra_cholesky -fopenmp
-echo "Enter n_max"
-read n_max
+# echo "Enter n_max"
+# read n_max
 echo "Enter threads"
 read max_num_threads
 echo "Enter amount of experiments"
 read experiments
 for (( experiment=1; experiment<=$experiments; experiment++ ))
 do
-	echo "Experiment #$experiment"
-	for (( num_threads=1; num_threads<=$max_num_threads; num_threads++ ))
-	do
-		export OMP_NUM_THREADS=$num_threads
-		echo "num_threads=$num_threads"
-		filename=../data/$experiment.$num_threads.txt
-		echo "# n; Execution time, sec" > $filename
-		for n in `seq 100 50 $n_max`
-		do
-			echo "$n 0" | ./linear_algebra_cholesky >> $filename
-		done
-	done
+   echo "Experiment #$experiment"
+   for (( num_threads=1; num_threads<=$max_num_threads; num_threads++ ))
+   do
+      export OMP_NUM_THREADS=$num_threads
+      echo "num_threads=$num_threads"
+      filename=../data/1/$experiment.$num_threads.txt
+      echo "# n; Execution time, sec" > $filename
+      for n in `seq 100 200 2500` #$n_max`
+      do
+         echo "n=$n"
+         echo "$n" | ./linear_algebra_cholesky >> $filename
+      done
+   done
 done
